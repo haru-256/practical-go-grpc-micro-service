904b8fcd714502835b0de79d083a08bff787e091
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-10-02 23:49:25 +0900

    refactor: Simplify product ID validation tests by removing redundant setup code

--- workbench/service/command/internal/domain/models/products/product_test.go
+++ workbench/service/command/internal/domain/models/products/product_test.go
@@ -15,36 +15,32 @@ func TestEntityPackage(t *testing.T) {
 }
 
 var _ = Describe("Productエンティティを構成する値オブジェクト", Ordered, Label("ProductId構造体の生成"), func() {
-	var emptyStr *errs.DomainError   // 空文字列
-	var lengthOver *errs.DomainError // 36文字以上
-	var notUUID *errs.DomainError    // UUID形式ではない
-	var productId *ProductId         // 正常系
-	var uid string
-
-	// 前処理
-	BeforeAll(func() {
-		_, err := NewProductId("")
-		emptyStr = err.(*errs.DomainError)
-		_, err = NewProductId("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-		lengthOver = err.(*errs.DomainError)
-		_, err = NewProductId("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-		notUUID = err.(*errs.DomainError)
-		id, _ := uuid.NewRandom()
-		uid = id.String()
-		productId, _ = NewProductId(uid)
-	})
-
 	// 文字数の検証
 	Context("商品IDの文字数の検証", Label("文字数"), func() {
+		_, err := NewProductId("")
+		emptyStr, ok := err.(*errs.DomainError)
+		Expect(ok).To(BeTrue())
 		It("空文字列の場合、エラーになること", func() {
 			Expect(emptyStr).To(Equal(errs.NewDomainError("INVALID_ARGUMENT", "商品IDの長さは36文字である必要があります")))
 		})
+
+		_, err = NewProductId("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		lengthOver, ok := err.(*errs.DomainError)
+		Expect(ok).To(BeTrue())
 		It("36文字以上の場合、エラーになること", func() {
 			Expect(lengthOver).To(Equal(errs.NewDomainError("INVALID_ARGUMENT", "商品IDの長さは36文字である必要があります")))
 		})
 	})
 	// UUID形式の検証
 	Context("商品IDのUUID形式の検証", Label("UUID形式"), func() {
+		_, err := NewProductId("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		notUUID, ok := err.(*errs.DomainError)
+		Expect(ok).To(BeTrue())
+
+		id, _ := uuid.NewRandom()
+		uid := id.String()
+		productId, _ := NewProductId(uid)
+
 		It("UUID形式ではない場合、エラーになること", func() {
 			Expect(notUUID).To(Equal(errs.NewDomainError("INVALID_ARGUMENT", "商品IDはUUIDの形式である必要があります")))
 		})
